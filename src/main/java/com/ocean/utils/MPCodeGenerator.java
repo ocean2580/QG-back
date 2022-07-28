8a96ae3448ceffcef58c90a290643a00ee77e587
Ocean <2080917477@qq.com>
2022-07-28 22:55:03 +0800

    代码生成器

---
+++ src/main/java/com/ocean/utils/MPCodeGenerator.java
@@ -0,0 +1,36 @@
+package com.ocean.utils;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
+
+import java.util.Collections;
+
+public class MPCodeGenerator {
+    public static void main(String[] args) {
+        generate();
+    }
+
+    private static void generate() {
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/test?serverTimezone=GMT%2b8",
+                "root", "DXH")
+                .globalConfig(builder -> {
+                    builder.author("ocean") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir("E:\\MyCode\\java\\back-test\\src\\main\\java\\"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.ocean") // 设置父包名
+                            .moduleName("") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "E:\\MyCode\\java\\back-test\\src\\main\\resources\\mapper")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("sys_user") // 设置需要生成的表名
+                            .addTablePrefix("t_", "sys_"); // 设置过滤表前缀
+                })
+//                .templateEngine(new VelocityTemplateEngine())
+                // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+}
