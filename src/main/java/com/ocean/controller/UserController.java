2de0270e39bc33310a8875cc7bd84c51aea1d5d7
Ocean <2080917477@qq.com>
2022-07-29 16:21:45 +0800

    导入导出功能实现

--- src/main/java/com/ocean/controller/UserController.java
+++ src/main/java/com/ocean/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.ocean.controller;
 
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ocean.entity.User;
 import com.ocean.mapper.UserMapper;
 import com.ocean.service.UserService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,8 +54,9 @@ public class UserController {
 
 
     /**
-     *  // @RequestParam 接受 ?pageNum=1&pageSize=10
-     * @param num = (pageNum-1) * size
+     * // @RequestParam 接受 ?pageNum=1&pageSize=10
+     *
+     * @param num  = (pageNum-1) * size
      * @param size = pageSize
      * @return 分页查询结果
      */
@@ -64,23 +76,69 @@ public class UserController {
 //    mybatis-plus 写法
     @GetMapping("/page")
     public IPage<User> findPage(@RequestParam("pageNum") Integer num,
-                                        @RequestParam("pageSize") Integer size,
-                                        @RequestParam(value = "username",defaultValue = "") String name,
-                                @RequestParam(value = "email",defaultValue = "") String email,
-                                @RequestParam(value = "address",defaultValue = "")String address) {
-        IPage<User> page = new Page<>(num,size);
+                                @RequestParam("pageSize") Integer size,
+                                @RequestParam(value = "username", defaultValue = "") String name,
+                                @RequestParam(value = "email", defaultValue = "") String email,
+                                @RequestParam(value = "address", defaultValue = "") String address) {
+        IPage<User> page = new Page<>(num, size);
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         // and 连接
         if (!"".equals(name)) {
-            queryWrapper.like("username",name);
+            queryWrapper.like("username", name);
         }
         if (!"".equals(email)) {
-            queryWrapper.like("email",email);
+            queryWrapper.like("email", email);
         }
         if (!"".equals(address)) {
-            queryWrapper.like("address",address);
+            queryWrapper.like("address", address);
         }
 //        queryWrapper.orderByDesc("id");
         return userService.page(page, queryWrapper);
+    }
+
+
+    /**
+     * 导出用户信息
+     */
+    @SneakyThrows
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) throws UnsupportedEncodingException {
+        List<User> list = userService.list();
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+
+//        转中文
+//        writer.addHeaderAlias("username", "用户名");
+//        writer.addHeaderAlias("password", "密码");
+//        writer.addHeaderAlias("email", "邮箱");
+//        writer.addHeaderAlias("address", "地址");
+//        writer.addHeaderAlias("nickname", "昵称");
+//        writer.addHeaderAlias("phone", "电话");
+//        writer.addHeaderAlias("createTime", "创建时间");
+//        writer.addHeaderAlias("avatarUrl", "头像");
+
+        writer.write(list, true);
+
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String fileName = URLEncoder.encode("用户信息", "UTF-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
+
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        out.close();
+        writer.close();
+
+    }
+
+    /**
+     * 导入
+     */
+    @SneakyThrows
+    @PostMapping("/import")
+    public void imp(MultipartFile file) throws IOException {
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+        List<User> users = reader.readAll(User.class);
+        System.out.println(users);
+        userService.saveOrUpdateBatch(users);
     }
 }
--- src/main/java/com/ocean/entity/User.java
+++ src/main/java/com/ocean/entity/User.java
@@ -8,11 +8,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName(value = "sys_user")
+@ToString
 public class User {
 
     @TableId(type = IdType.AUTO)
@@ -26,5 +30,7 @@ public class User {
     private String address;
     @TableField("avatar_url")   // 自动将 下划线 转为 驼峰
     private String avatar;
+    @TableField("create_time")
+    private LocalTime createTime;
 
 }
