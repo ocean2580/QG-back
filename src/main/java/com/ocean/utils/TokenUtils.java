3a842e36515147437f8584f3802c9cc88e9a2394
Ocean <2080917477@qq.com>
2022-07-30 11:57:09 +0800

    拿到当前user

--- src/main/java/com/ocean/utils/TokenUtils.java
+++ src/main/java/com/ocean/utils/TokenUtils.java
@@ -1,18 +1,55 @@
 package com.ocean.utils;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.ocean.entity.User;
+import com.ocean.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
-
+@Component
 public class TokenUtils {
 
+    private static UserService staticUserService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void setUserService() {
+        staticUserService = userService;
+    }
+
+    // 生成token
     public static String generateToken(String userId, String sign) {
         return JWT.create().withAudience(userId) // 将 user id 保存到 token 里面
-                .withExpiresAt(DateUtil.offsetHour(new Date(),2)) // 2h后token过期
+                .withExpiresAt(DateUtil.offsetHour(new Date(), 2)) // 2h后token过期
                 .sign(Algorithm.HMAC256(sign)); // 以 sign 作为 token 的密钥
 
+    }
+
+
+    // 拿到当前user
+    public static User getCurrentUser() {
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String token = request.getHeader("token");
+            if (StrUtil.isNotBlank(token)) {
+                String userId = JWT.decode(token).getAudience().get(0);
+                return staticUserService.getById(Integer.valueOf(userId));
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
     }
 }
