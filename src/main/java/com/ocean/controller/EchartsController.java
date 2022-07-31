c7fab62e7ee866e62cf4297655b9a26c3cbb7e01
Ocean <2080917477@qq.com>
2022-07-31 15:48:11 +0800

    集成Echarts实现首页 柱状|折线|饼状 统计图 数据从后台传

---
+++ src/main/java/com/ocean/controller/EchartsController.java
@@ -0,0 +1,55 @@
+package com.ocean.controller;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.Quarter;
+import com.ocean.common.Result;
+import com.ocean.entity.User;
+import com.ocean.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/echarts")
+public class EchartsController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/example")
+    public Result get() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("x", CollUtil.newArrayList("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"));
+        map.put("y", CollUtil.newArrayList(150, 230, 224, 218, 135, 147, 260));
+        return Result.success(map);
+    }
+
+    @GetMapping("/members")
+    public Result members() {
+        List<User> list = userService.list();
+        int s1 = 0; // 季度数
+        int s2 = 0;
+        int s3 = 0;
+        int s4 = 0;
+
+        for (User user : list) {
+            Date createTime = user.getCreateTime();
+            Quarter quarter = DateUtil.quarterEnum(createTime);
+            switch (quarter) {
+                case Q1: s1 += 1; break;
+                case Q2: s2 += 1; break;
+                case Q3: s3 += 1; break;
+                case Q4: s4 += 1; break;
+                default:break;
+            }
+        }
+        return Result.success(CollUtil.newArrayList(s1,s2,s3,s4));
+    }
+}
