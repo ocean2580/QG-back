6c73fa63e6bd7c9d2520c01508deda28110396ed
Ocean <2080917477@qq.com>
2022-08-26 18:17:16 +0800

    更正

--- src/main/java/com/ocean/config/InterceptorConfig.java
+++ src/main/java/com/ocean/config/InterceptorConfig.java
@@ -8,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class InterceptorConfig implements WebMvcConfigurer{
-
+public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor())
                 .addPathPatterns("/**")     // 拦截所有请求，通过判断是否有 @LoginRequired 注解 决定是否需要登录
-                .excludePathPatterns("/user/login","/user/register","/**/import","/**/export","/files/**");
+                .excludePathPatterns("/user/login","/user/register","/**/import","/**/export","/**/file/**");
 
+        //TODO 文件可上传存储，但是无法下载
 
         //注册TestInterceptor拦截器
 //        InterceptorRegistration registration = registry.addInterceptor(jwtInterceptor());
